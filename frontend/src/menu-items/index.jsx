aa3e62c11e80c10b296f31eb3eee00ae9559019f
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-10-02 11:51:36 -0300

    fix: elimine items del menu que no se usan

--- frontend/src/menu-items/index.jsx
+++ frontend/src/menu-items/index.jsx
@@ -1,13 +1,11 @@
 // project import
-import dashboard from './dashboard';
-import pages from './page';
 import utilities from './utilities';
-import support from './support';
+
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [dashboard, pages, utilities, support]
+  items: [ utilities]
 };
 
 export default menuItems;
