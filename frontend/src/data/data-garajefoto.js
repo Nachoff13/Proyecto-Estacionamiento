955ec7482f0321ffe5fa2fa8920c84bf2f7f9968
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-02 18:30:42 -0300

    feat: unifiqué fakes datas

--- frontend/src/data/data-garajefoto.js
+++ frontend/src/data/data-garajefoto.js
@@ -1,23 +1,40 @@
 import garaje1 from 'assets/images/home/garajes-propietario/garaje1.jpg';
 import garaje2 from 'assets/images/home/garajes-propietario/garaje2.jpg';
+import garaje55 from 'assets/images/home/listado-garaje/garaje1.jpg';
+import garaje56 from 'assets/images/home/listado-garaje/garaje2.jpg';
+import garaje57 from 'assets/images/home/listado-garaje/garaje3.jpeg';
 
 const fakeFotosGarajes = [
-    {
-      id: 1,
-      idGaraje: 1, // Asumiendo que el garage con id 1 tiene esta foto
-      foto: garaje1
-    },
-    {
-      id: 2,
-      idGaraje: 2, // Otro ejemplo de foto para el garage con id 1
-      foto: garaje2
-    },
-    // {
-    //   id: 3,
-    //   idGaraje: 2, // Foto para el garage con id 2
-    //   foto: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD...' // Representación base64 de una imagen
-    // }
-  ];
-  
+  {
+    id: 1,
+    idGaraje: 1, // Asumiendo que el garage con id 1 tiene esta foto
+    foto: garaje1
+  },
+  {
+    id: 2,
+    idGaraje: 2, // Otro ejemplo de foto para el garage con id 1
+    foto: garaje2
+  },
+  {
+    id: 3,
+    idGaraje: 55,
+    foto: garaje55
+  },
+  {
+    id: 4,
+    idGaraje: 56,
+    foto: garaje56
+  },
+  {
+    id: 5,
+    idGaraje: 57,
+    foto: garaje57
+  },
+  {
+    id: 6,
+    idGaraje: 58,
+    foto: garaje57
+  }
+];
+
 export default fakeFotosGarajes;
-  
--- frontend/src/data/data-garajes.js
+++ frontend/src/data/data-garajes.js
@@ -1,27 +1,70 @@
 const fakeGarajes = [
-    {
-      id: 1,
-      idPropietario: 3, // Este garage pertenece al propietario con id 3
-      idLocalidad: 1,    // Asumiendo que 1 representa La Plata, puedes ajustar según necesites
-      idGarajeEstado: 1, 
-      altura: '1234',
-      calle: 'Calle 10',
-      descripcion: 'Garage amplio con cámaras de seguridad, ideal para autos grandes.',
-      precioHora: 150.50,
-      capacidad: 1,
-    },
-    {
-      id: 2,
-      idPropietario: 3, // Segundo garage del propietario con id 3
-      idLocalidad: 1,    // Asumiendo que 1 representa La Plata
-      idGarajeEstado: 2,
-      altura: '760',
-      calle: 'Calle 56',
-      descripcion: 'Garage techado, cerca de áreas comerciales.',
-      precioHora: 200.75,
-      capacidad: 1,
-    }
-  ];
-  
-  export default fakeGarajes;
-  
+  {
+    id: 1,
+    idPropietario: 3, // Este garage pertenece al propietario con id 3
+    idLocalidad: 1, // Asumiendo que 1 representa La Plata, puedes ajustar según necesites
+    idGarajeEstado: 1,
+    altura: '1234',
+    calle: 'Calle 10',
+    descripcion: 'Garage amplio con cámaras de seguridad, ideal para autos grandes.',
+    precioHora: 150.5,
+    capacidad: 1
+  },
+  {
+    id: 2,
+    idPropietario: 3, // Segundo garage del propietario con id 3
+    idLocalidad: 1, // Asumiendo que 1 representa La Plata
+    idGarajeEstado: 2,
+    altura: '760',
+    calle: 'Calle 56',
+    descripcion: 'Garage techado, cerca de áreas comerciales.',
+    precioHora: 200.75,
+    capacidad: 1
+  },
+  {
+    id: 55,
+    idPropietario: 2,
+    idLocalidad: 1,
+    idGarajeEstado: 1,
+    altura: 15,
+    Calle: 'Av 7 entre 16 y 17',
+    descripcion: 'Garaje amplio, techado y seguro.',
+    precioHora: 50,
+    capacidad: 4,
+  },
+  {
+    id: 56,
+    idPropietario: 2,
+    idLocalidad: 1,
+    idGarajeEstado: 1,
+    altura: 22,
+    Calle: 'Calle 530 entre 48 y 49',
+    descripcion: 'Garaje pequeño, ideal para motocicletas.',
+    precioHora: 30,
+    capacidad: 1
+  },
+  {
+    id: 57,
+    idPropietario: 6,
+    idLocalidad: 1,
+    idGarajeEstado: 1,
+    altura: 10,
+    Calle: 'Av 13',
+    descripcion: 'Garaje espacioso con vigilancia 24 horas.',
+    precioHora: 60,
+    capacidad: 5
+  },
+  {
+    id: 58,
+    idPropietario: 6,
+    idLocalidad: 1,
+    idGarajeEstado: 2,
+    altura: 10,
+    Calle: 'Av 32',
+    descripcion: 'Garaje espacioso con vigilancia 24 horas.',
+    precioHora: 60,
+    capacidad: 5
+  }
+];
+
+export default fakeGarajes;
