8afa4f5e0d63474d3adecde79d9df3247d869b77
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-02 19:48:16 -0300

    feat: actualizacion de la funcion del switch para q cambie segun rol

--- frontend/src/components/ModeSwitch.jsx
+++ frontend/src/components/ModeSwitch.jsx
@@ -1,21 +1,49 @@
-// src/components/ModoSwitch.jsx
-
 import React from 'react';
-import { Switch } from 'antd'; // Asegúrate de que 'antd' esté instalado
+import { Switch } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-const ModeSwitch = ({ onModeChange }) => {
-  // Estado inicial del modo (puedes cambiarlo a true o false según tus necesidades)
-  const [modo, setModo] = React.useState(false);
+const ModeSwitch = ({ usuario }) => {
+  const [modo, setModo] = React.useState(false); // Estado inicial del switch
+  const navigate = useNavigate(); // Hook para navegar programáticamente
 
+  // cambia la pagina segun el modo
   const handleSwitchChange = (checked) => {
     setModo(checked);
-    onModeChange(checked); // Llama a la función de cambio de modo pasándolo al padre
+
+    if (checked) {
+      navigate('/home-conductor'); // Navega a la página del conductor
+    } else {
+      navigate('/home-propietario'); // Navega a la página del propietario
+    }
   };
 
+
+  React.useEffect(() => {
+    if (usuario.esPropietario && usuario.esConductor) {
+      // es propietario y conductor
+      setModo(false); 
+    } else if (!usuario.esPropietario && usuario.esConductor) {
+      //es solo conductor
+      setModo(true);
+      navigate('/home-conductor');
+    } else if (usuario.esPropietario && !usuario.esConductor) {
+      // es solo propietario
+      setModo(false);
+      navigate('/home-propietario');
+    }
+  }, [usuario, navigate]);
+
+  // deshabilita el switch
+  const isDisabled = (!usuario.esPropietario && usuario.esConductor) || (usuario.esPropietario && !usuario.esConductor);
+
   return (
-    <div style={{ padding: '16px', marginLeft:'5px' }}>
-      <span style={{marginRight: '5px'}}>Modo {modo ? 'Conductor' : 'Propietario'}</span>
-      <Switch checked={modo} onChange={handleSwitchChange} />
+    <div style={{ padding: '16px', marginLeft: '5px' }}>
+      <span style={{ marginRight: '5px' }}>Modo {modo ? 'Conductor' : 'Propietario'}</span>
+      <Switch
+        checked={modo}
+        onChange={handleSwitchChange}
+        disabled={isDisabled} 
+      />
     </div>
   );
 };
