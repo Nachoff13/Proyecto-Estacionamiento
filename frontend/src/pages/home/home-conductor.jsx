caf4874f492a9c2a51cdd042c70e68475c66f760
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-02 18:31:06 -0300

    feat: mejoré tarjeta de garaje

--- frontend/src/pages/home/home-conductor.jsx
+++ frontend/src/pages/home/home-conductor.jsx
@@ -1,41 +1,98 @@
 // material-ui
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Chip } from '@mui/material';
 
 // project import
 import MainCard from 'components/MainCard';
 
-import dataGarajes from './data-garaje';
+//data imports
+import dataGarajes from 'data/data-garajes';
+import fakeFotosGarajes from 'data/data-garajefoto';
+import fakeLocalidades from 'data/data-localidades';
+import fakeGarajeEstado from 'data/data-garajeestado';
+import fakePropietario from 'data/data-usuarios';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 export default function HomeConductor() {
+  const getLocalidad = (idLocalidad) => {
+    const localidad = fakeLocalidades.find((loc) => loc.id === idLocalidad);
+    return localidad ? localidad.nombre : 'Desconocido';
+  };
+
+  const getEstadoGaraje = (idGarajeEstado) => {
+    const estado = fakeGarajeEstado.find((est) => est.id === idGarajeEstado);
+    return estado ? estado.nombre : 'Desconocido';
+  };
+
+  const getFotoGaraje = (idGaraje) => {
+    const fotoGaraje = fakeFotosGarajes.find((foto) => foto.idGaraje === idGaraje);
+    return fotoGaraje ? fotoGaraje.foto : 'default-foto.jpg'; // En caso de que no encuentre foto
+  };
+
+  const getPropietario = (idPropietario) => {
+    const propietario = fakePropietario.find((propietario) => propietario.id === idPropietario);
+    return propietario ? propietario.nombre + ' ' + propietario.apellido : 'Desconocido';
+  };
+
+  //-------------------------------GET COLOR ESTADO-----------------------------------
+  const getColorEstado = (idGarajeEstado) => {
+    switch (idGarajeEstado) {
+      case 1: // Disponible
+        return 'success';
+      case 2: // Reservado
+        return 'warning';
+      case 3: // No Disponible
+        return 'error';
+      default:
+        return 'default';
+    }
+  };
+
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%' }}>
-      {dataGarajes.map((garaje) => (
-        <MainCard key={garaje.id} title={`Garaje Disponible ${garaje.id}`} style={{ width: '33.33%', boxSizing: 'border-box',  height: '500px' }}>
-          <div style={{ flex: '1', overflow: 'hidden' }}> 
+      {dataGarajes
+        .filter((garaje) => garaje.idGarajeEstado === 1) // Filtramos los garajes que estan dispo
+        .map((garaje, index) => (
+          <MainCard key={garaje.id} style={{ width: '33.33%', boxSizing: 'border-box', height: '450px' }}>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Typography variant="h6" style={{ marginBottom: '8px' }}>
+                Garaje {index + 1}
+              </Typography>
+              <Chip
+                label={getEstadoGaraje(garaje.idGarajeEstado)}
+                color={getColorEstado(garaje.idGarajeEstado)}
+                variant="outlined"
+                style={{ marginBottom: '8px' }}
+              />
+            </div>
             <img
-              src={garaje.garajeFoto.src} // Usa la imagen desde los datos
-              alt={garaje.garajeFoto.alt}
+              src={getFotoGaraje(garaje.id)} // Obtener la imagen del garaje
+              alt={`Foto del garaje ${garaje.id}`}
               style={{ width: '100%', height: '200px', objectFit: 'cover' }}
             />
-            <Typography variant="body2">
-              <strong>Propietario: </strong> {garaje.idPropietario} <br />
-              <strong>Localidad: </strong> {garaje.idLocalidad} <br />
-              <strong>Ubicación:</strong> {garaje.Calle} N° {garaje.altura} <br />
-              <strong>Descripción:</strong> {garaje.descripcion} <br />
-              <strong>Precio por hora:</strong> ${garaje.precioHora} <br />
-              <strong>Capacidad:</strong> {garaje.capacidad} vehículos <br />
-              <strong>Estado:</strong> {garaje.estado} <br />
-            </Typography>
-          </div>  
-            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              <Button variant="contained" color="primary"  onClick={() => handleReservar(garaje.id)}  style={{ marginTop: '10px' }}>
+            <div style={{ flex: '1', overflow: 'hidden' }}>
+              <Typography variant="body2">
+                <strong>Propietario: </strong> {getPropietario(garaje.idPropietario)} <br />
+                <strong>Localidad: </strong> {getLocalidad(garaje.idLocalidad)} <strong>Ubicación:</strong> {garaje.Calle} N°{' '}
+                {garaje.altura} <br />
+                <strong>Descripción:</strong> {garaje.descripcion} <br />
+                <strong>Precio por hora:</strong> ${garaje.precioHora} <br />
+                <strong>Capacidad:</strong> {garaje.capacidad} vehículos <br />
+              </Typography>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: "auto" }}>
+              <Button variant="contained" color="primary" onClick={() => handleReservar(garaje.id)} style={{ marginTop: '10px' }}>
                 Reservar
               </Button>
             </div>
-        </MainCard>
-      ))}
+          </MainCard>
+        ))}
     </div>
   );
 }
