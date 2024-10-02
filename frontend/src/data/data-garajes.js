aba5718b70edc5e2fa6d6db55d149883350ca406
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-10-02 10:37:44 -0300

    feat: agregué fakeDatas

---
+++ frontend/src/data/data-garajes.js
@@ -0,0 +1,27 @@
+const fakeGarages = [
+    {
+      id: 1,
+      idPropietario: 3, // Este garage pertenece al propietario con id 3
+      idLocalidad: 1,    // Asumiendo que 1 representa La Plata, puedes ajustar según necesites
+      altura: '1234',
+      calle: 'Calle Falsa',
+      descripcion: 'Garage amplio con cámaras de seguridad, ideal para autos grandes.',
+      precioHora: 150.50,
+      capacidad: 2,
+      Estado: 'Disponible'
+    },
+    {
+      id: 2,
+      idPropietario: 3, // Segundo garage del propietario con id 3
+      idLocalidad: 1,    // Asumiendo que 1 representa La Plata
+      altura: '5678',
+      calle: 'Avenida Siempreviva',
+      descripcion: 'Garage techado, cerca de áreas comerciales.',
+      precioHora: 200.75,
+      capacidad: 3,
+      Estado: 'Ocupado'
+    }
+  ];
+  
+  export default fakeGarages;
+  
---
+++ frontend/src/data/data-localidades.js
@@ -0,0 +1,23 @@
+const fakeLocalidades = [
+    {
+      id: 1,
+      idProvincia: 1, // Asumiendo que 1 representa la Provincia de Buenos Aires
+      nombre: 'La Plata',
+      codpostal: '1900'
+    },
+    {
+      id: 2,
+      idProvincia: 1, 
+      nombre: 'Berisso',
+      codpostal: '1923'
+    },
+    {
+      id: 3,
+      idProvincia: 1,
+      nombre: 'Ensenada',
+      codpostal: '1925'
+    }
+  ];
+  
+  export default fakeLocalidades;
+  
