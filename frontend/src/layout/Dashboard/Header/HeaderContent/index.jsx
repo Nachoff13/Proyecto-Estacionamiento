a2bea4f8c784153983b6c2b9afbe54b49edf3c0c
JuanbyGuada <juanby2009@yahoo.com.ar>
2024-10-02 21:22:23 -0300

    acceso al repo en header

--- frontend/src/layout/Dashboard/Header/HeaderContent/index.jsx
+++ frontend/src/layout/Dashboard/Header/HeaderContent/index.jsx
@@ -24,11 +24,11 @@ export default function HeaderContent() {
       {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
       <IconButton
         component={Link}
-        href="https://github.com/codedthemes/mantis-free-react-admin-template"
+        href="https://github.com/Nachoff13/Proyecto-Estacionamiento"
         target="_blank"
         disableRipple
         color="secondary"
-        title="Download Free Version"
+        title="Repo"
         sx={{ color: 'text.primary', bgcolor: 'grey.100' }}
       >
         <GithubOutlined />
