7614d9ad6a3469064cd56b1451e2fe8ec745b4b8
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-10-28 12:13:33 -0300

    feat: agregué controlador para Usuario

---
+++ Backend/Backend_Estacionamiento/Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.Servicios;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuario _usuario;
+
+        public UsuarioController(IUsuario usuario)
+        {
+            _usuario = usuario;
+        }
+
+        [HttpPost("Agregar")]
+        public async Task<ActionResult<int>> Agregar(Core.DTO.UsuarioDTO usuario)
+        {
+            var resultado = await _usuario.Agregar(usuario).ConfigureAwait(false);
+            return Ok(resultado);
+        }
+
+        [HttpPut("Modificar")]
+        public async Task<ActionResult<bool>> Modificar(UsuarioConId usuario)
+        {
+            var resultado = await _usuario.Modificar(usuario).ConfigureAwait(false);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("Eliminar/{id}")]
+        public async Task<ActionResult<bool>> Eliminar(int id)
+        {
+            var resultado = await _usuario.Eliminar(id).ConfigureAwait(false);
+            return Ok(resultado);
+        }
+
+        [HttpGet("Obtener")]
+        public async Task<ActionResult<List<UsuarioConId>>> Obtener()
+        {
+            var resultados = await _usuario.Obtener().ConfigureAwait(false);
+            return Ok(resultados);
+        }
+
+        [HttpGet("ObtenerIndividual/{id}")]
+        public async Task<ActionResult<UsuarioConId>> ObtenerIndividual(int id)
+        {
+            var resultado = await _usuario.ObtenerIndividual(id).ConfigureAwait(false);
+            return Ok(resultado);
+        }
+    }
+}
