2c25730b858c38d6635113cc3cc2cb2a2d1d8502
Nuria Robledo <nuriarobledo0@gmail.com>
2024-11-13 15:07:31 -0300

    feat: que el menú solo muestre lo necesario para cada usuario

--- frontend/src/menu-items/utilities.jsx
+++ frontend/src/menu-items/utilities.jsx
@@ -20,35 +20,52 @@ const icons = {
   CarOutlined
 };
 
-// ==============================|| MENU ITEMS - UTILITIES ||============================== //
+// data
+import fakeUsuarios from 'data/data-usuarios';
 
-const utilities = {
+// ==============================|| MENU ITEMS - UTILITIES ||============================== //
+// Simula obtener el usuario actual
+const usuarioId = 3; // Juan Perez, Propietario
+// const usuarioId = 4; // Martin Suarez, Conductor
+// const usuarioId = 6; // David Torres, Conductor y Propietario
+const usuario = fakeUsuarios.find((user) => user.id === usuarioId);
+
+let utilities = {
   id: 'utilities',
   title: 'Utilidades',
   type: 'group',
-  children: [
-    {
+  children: []
+};
+
+// Construir el menú basado en el rol del usuario
+if (usuario) {
+  if (usuario.esPropietario) {
+    utilities.children.push({
+      id: 'home-propietario',
+      title: 'Mis Garajes',
+      type: 'item',
+      url: '/home-propietario',
+      icon: icons.CarOutlined 
+    });
+  }
+  
+  if (usuario.esConductor) {
+    utilities.children.push({
       id: 'home-conductor',
       title: 'Listado de Garajes',
       type: 'item',
       url: '/home-conductor',
       icon: icons.CarOutlined
-    },
-    {
-      id: 'home-propietario',
-      title: 'Mis Garajes',
-      type: 'item',
-      url: '/home-propietario',
-      icon: CarOutlined
-    },
-    {
+    });
+
+    utilities.children.push({
       id: 'alta-vehiculo',
       title: 'Alta de Vehículo',
       type: 'item',
       url: '/alta-vehiculo',
-      icon: CarOutlined
-    }
-  ]
-};
+      icon: icons.CarOutlined 
+    });
+  }
+}
 
 export default utilities;
