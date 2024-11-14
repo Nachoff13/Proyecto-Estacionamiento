2eab03b4d1be5d596cea24cadea86ff2b8cd257f
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-11-14 19:40:36 -0300

    feat: actualice para que me deveuvla un array y elimine comentarios

--- frontend/src/api/Usuario.js
+++ frontend/src/api/Usuario.js
@@ -1,6 +1,6 @@
 import useSWR, { mutate } from 'swr';
 import { useMemo } from 'react';
-import { getUsuarios} from './api-Usuario';
+import { getPropietarios, getUsuarios} from './api-Usuario';
 
 
 const initialState = {
@@ -12,47 +12,23 @@ const initialState = {
 export const endpoints = {
   key: 'https://localhost:7294',
   actions: 'actions',
-  list: '/Usuario/Obtener', // server URL
+  list: '/Usuario/ObtenerPropietario' // server URL
 };
 
-export function useGetUsuario() {
-    const { data, error, isValidating } = useSWR(endpoints.key + endpoints.list, getUsuarios);
-  
-    // Agregar console.log para depuración
-    console.log('Fetching Usuario from:', endpoints.key + endpoints.list);
-    console.log('Data Usuario:', data);
-  
+//DEVUELVE EL ARRAY DE PROPIETARIOS
+export function useGetPropietario() {
+    const { data, error, isValidating } = useSWR(endpoints.key + endpoints.list, getPropietarios);
     const memoizedValue = useMemo(
       () => ({
-        usuario: data?.datos || [], 
-        usuarioLoading: !error && !data,
-        usuarioError: error,
-        usuarioValidating: isValidating,
-        usuarioEmpty: data?.datos ? data.datos.length === 0 : true
+        propietario: data|| [], 
+        propietarioLoading: !error && !data,
+        propietarioError: error,
+        propietarioValidating: isValidating,
+        propietarioEmpty: data ? data.length === 0 : true
       }),
       [data, error, isValidating]
     );
+
     return memoizedValue;
   }
 
-  export function useGetPropietario() {
-    const { data, error, isValidating } = useSWR(endpoints.key + endpoints.list, getUsuarios);
-    
-    // Agregar console.log para depuración
-    console.log('Fetching Usuario from:', endpoints.key + endpoints.list);
-    console.log('Data Propietario:', data);
-    
-    const memoizedValue = useMemo(
-      () => ({
-        usuarios: data?.datos || [],
-        usuariosLoading: !error && !data,
-        usuariosError: error,
-        usuariosValidating: isValidating,
-        usuariosEmpty: data?.datos ? data.datos.length === 0 : true,
-        propietarios: (data?.datos || []).filter(usuario => usuario.espropietario) || [], // Filtra los propietarios
-      }),
-      [data, error, isValidating]
-    );
-  
-    return memoizedValue;
-  }
