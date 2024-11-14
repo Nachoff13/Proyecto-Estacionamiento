2101b1b6063f7894d143ef1b00741f8636fbc71c
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-11-14 19:38:52 -0300

    feat: actualice la api de garajefoto

--- frontend/src/api/api-GarajeFoto.js
+++ frontend/src/api/api-GarajeFoto.js
@@ -1,3 +1,4 @@
+// api-GarajeFoto.js
 import axios from 'axios';
 
 const api = axios.create({
@@ -6,13 +7,26 @@ const api = axios.create({
     headers: {
       'Content-Type': 'application/json'
     }
-  });
+});
 
-export const getGarajeFotos = async () => {
+export const getGarajeFotos = async (idGaraje) => {
   try {
-    const response = await api.get('/GarajeFoto/Obtener');
-    return response.data;
+    const response = await api.get(`/GarajeFoto/ObtenerFotosDeGaraje/${idGaraje}`);
+    console.log("response data fotogarajes:", response.data);
+    
+    const imagesData = response.data.map(img => {
+      const byteArray = new Uint8Array(img.foto.split(',').map(Number));
+      const blob = new Blob([byteArray], { type: 'image/jpeg' });
+      return {
+        id: img.id,
+        idgaraje: img.idgaraje,
+        fotoUrl: URL.createObjectURL(blob)
+      };
+    });
+    
+    return imagesData;
   } catch (error) {
     console.error("Error fetching GarajeFoto:", error);
+    return [];  // Retornar array vacío en caso de error para evitar problemas en el componente
   }
-}
+};
