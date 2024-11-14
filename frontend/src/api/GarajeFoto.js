68d89e0f0cc44f60e22b08b58d375b08c3c05900
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-11-14 19:39:53 -0300

    feat: actualice useGetGarajeFoto

--- frontend/src/api/GarajeFoto.js
+++ frontend/src/api/GarajeFoto.js
@@ -1,36 +1,24 @@
-import useSWR, { mutate } from 'swr';
+// useGarajeFoto.js
+import useSWR from 'swr';
 import { useMemo } from 'react';
-import { getGarajeFotos} from './api-GarajeFoto';
+import { getGarajeFotos } from './api-GarajeFoto';
 
+// Hook para obtener las fotos del garaje
+export function useGetGarajeFoto(idGaraje) {
+    const { data, error, isValidating } = useSWR(idGaraje ? `/GarajeFoto/ObtenerFotosDeGaraje/${idGaraje}` : null, () => getGarajeFotos(idGaraje));
 
-const initialState = {
-  isOpen: false,
-  open: false,
-  alertPopup: false,
-};
+    console.log('Data Garaje Fotos:', data);
 
-export const endpoints = {
-  key: 'https://localhost:7294',
-  actions: 'actions',
-  list: '/GarajeFoto/Obtener', // server URL
-};
-
-export function useGetGarajeFoto() {
-    const { data, error, isValidating } = useSWR(endpoints.key + endpoints.list, getGarajeFotos);
-  
-    // Agregar console.log para depuración
-    console.log('Fetching garajesfotos from:', endpoints.key + endpoints.list);
-    console.log('Data Gataje foto:', data);
-  
     const memoizedValue = useMemo(
       () => ({
-        garajeFoto: data?.datos || [], 
-        garajeFotoLoading: !error && !data,
-        garajeFotoError: error,
-        garajeFotoValidating: isValidating,
-        garajeFotoEmpty: data?.datos ? data.datos.length === 0 : true
+        garajeFotos: data || [],
+        garajeFotosLoading: !error && !data,
+        garajeFotosError: error,
+        garajeFotosValidating: isValidating,
+        garajeFotosEmpty: data ? data.length === 0 : true
       }),
       [data, error, isValidating]
     );
+
     return memoizedValue;
-  }
+}
