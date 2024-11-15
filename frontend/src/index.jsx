40c4035612ef0f97aab47c6d44b33846f8f4ea3a
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-11-15 17:18:24 -0300

    feat: envuelvo a la app con el login

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 // scroll bar
 import 'simplebar-react/dist/simplebar.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 
 // google-fonts
 import '@fontsource/roboto/400.css';
@@ -12,30 +14,43 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/700.css';
 
+
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
+
 
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import '@fontsource/poppins/600.css';
 import '@fontsource/poppins/700.css';
 
+
 import '@fontsource/public-sans/400.css';
 import '@fontsource/public-sans/500.css';
 import '@fontsource/public-sans/600.css';
 import '@fontsource/public-sans/700.css';
 
+
 // project import
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from 'src/contexts/auth-reducer/UserContext.jsx'; // Asegúrate de que la ruta sea correcta
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
 // ==============================|| MAIN - REACT DOM RENDER ||============================== //
 
-root.render(<App />);
+
+root.render(
+  <UserProvider>
+    <App />
+  </UserProvider>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
