8169369245232b47867c5711d4684166702bd45a
Aylen <aylentastaca@gmail.com>
2024-11-15 18:39:11 -0300

    Se agrega la api opencage y se usa leaftlet para marcar las ubicaciones. Se agrega el mapa el home Conductor

--- frontend/src/pages/home/home-conductor.jsx
+++ frontend/src/pages/home/home-conductor.jsx
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from 'react';
 // material-ui
-import { Typography, Button, Chip, ImageList, ImageListItem } from '@mui/material';
+import { Typography, Button, Chip, ImageList, ImageListItem, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 // project import
 import MainCard from 'components/MainCard';
 import SearchBar from 'components/home/SearchBar';
+import MapView from 'components/MapView'; 
 
 // API imports
 import { useGetLocalidad } from 'src/api/Localidad';
+import { useGetProvincia } from 'src/api/Provincia';
 import { useGetPropietario } from 'src/api/Usuario';
 import { useGetGaraje } from 'api/Garaje';
 
 export default function HomeConductor() {
+  const [isDataLoaded, setIsDataLoaded] = useState(false); 
   const navigate = useNavigate();
   const [selectedLocalidad, setSelectedLocalidad] = useState(''); // Buscador
   // Estado para almacenar las fotos de cada garaje
   const [garageImages, setGarageImages] = useState({});
 
   // Obtener datos de API
+  const { provincia, provinciaLoading, provinciaError } = useGetProvincia();
+  console.log("Primero provincias:", provincia);
   const { localidad, localidadLoading, localidadError } = useGetLocalidad();
+  console.log("Primero localidades", localidad)
   const { propietario, propietarioLoading, propietarioError } = useGetPropietario();
   const { garaje, garajeLoading, garajeError } = useGetGaraje();
 
@@ -57,15 +63,23 @@ export default function HomeConductor() {
     }
   }, [garaje]);
 
+  useEffect(() => {
+    // Verifica cuando las provincias y localidades estén listas
+    if (!provinciaLoading && !localidadLoading) {
+      setIsDataLoaded(true);
+    }
+  }, [provinciaLoading, localidadLoading]);
+
   // Manejo de errores
-  if (localidadError || propietarioError || garajeError) {
+  if (provinciaError || localidadError || propietarioError || garajeError) {
     return <div>Error al cargar los datos.</div>;
   }
 
   // Manejo de carga
-  if (localidadLoading || propietarioLoading || garajeLoading) {
+  if (provinciaLoading || localidadLoading || propietarioLoading || garajeLoading) {
     return <div>Cargando...</div>;
   }
+
 
   // Función para obtener la localidad por ID
   const getLocalidad = (idLocalidad) => {
@@ -85,25 +99,33 @@ export default function HomeConductor() {
   };
 
   return (
-    <div>
+    <div style={{ display: 'flex', height: '100vh', gap: '16px' }}>
+
+      {/* Contenedor de las tarjetas */}
+      <Box style={{ flex: 6, overflowY: 'auto', padding: '16px' }}>
       <SearchBar onSearch={handleSearch} />
-      <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', gap: '16px' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
         {garaje
-          .filter((garageItem) => garageItem.disponible && (!selectedLocalidad || garageItem.idlocalidad === selectedLocalidad))
+          .filter(
+            (garageItem) =>
+              garageItem.disponible &&
+              (!selectedLocalidad || garageItem.idlocalidad === selectedLocalidad)
+          )
           .map((garageItem, index) => (
             <MainCard
               key={garageItem.id}
               style={{
-                width: '32%',
-                height: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                position: 'relative',
-                overflow: 'hidden',
-                padding: '16px'
+                flex: '0 1 calc(50% - 16px)',
+                marginBottom: '16px',
               }}
             >
-              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}
+              >
                 <Typography variant="h6" style={{ marginBottom: '8px' }}>
                   Garaje {index + 1}
                 </Typography>
@@ -114,48 +136,63 @@ export default function HomeConductor() {
                   style={{ marginBottom: '8px' }}
                 />
               </div>
-              <div style={{ flex: '1', overflow: 'hidden', marginBottom: '8px' }}>
-                <ImageList cols={1} rowHeight={150} style={{ width: '100%', height: 'auto' }}>
-                  {(garageImages[garageItem.id] || []).map((src, idx) => (
-                    <ImageListItem key={idx}>
-                      <img
-                        src={src}
-                        alt={`Foto ${idx + 1} del garaje ${garageItem.id}`}
-                        style={{ objectFit: 'contain', width: '100%', height: '100%', borderRadius: '8px' }}
-                      />
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-              </div>
-              <div style={{ flex: '1', overflow: 'hidden' }}>
-                <Typography variant="body2">
-                  <strong>Propietario:</strong> {getPropietario(garageItem.idpropietario)}
-                  <br />
-                  <strong>Localidad:</strong> {getLocalidad(garageItem.idlocalidad)}
-                  <br />
-                  <strong>Ubicación:</strong> {garageItem.calle} N° {garageItem.altura}
-                  <br />
-                  <strong>Descripción:</strong> {garageItem.descripcion}
-                  <br />
-                  <strong>Precio por hora:</strong> ${garageItem.preciohora}
-                  <br />
-                  <strong>Capacidad:</strong> {garageItem.capacidad} vehículos
-                  <br />
-                </Typography>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
+              <ImageList
+                cols={1}
+                rowHeight={150}
+                style={{ width: '100%', height: 'auto' }}
+              >
+                {(garageImages[garageItem.id] || []).map((src, idx) => (
+                  <ImageListItem key={idx}>
+                    <img
+                      src={src}
+                      alt={`Foto ${idx + 1} del garaje ${garageItem.id}`}
+                      style={{
+                        objectFit: 'contain',
+                        width: '100%',
+                        height: '100%',
+                        borderRadius: '8px',
+                      }}
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+              <Typography variant="body2">
+                <strong>Propietario:</strong> {getPropietario(garageItem.idpropietario)}
+                <br />
+                <strong>Localidad:</strong> {getLocalidad(garageItem.idlocalidad)}
+                <br />
+                <strong>Ubicación:</strong> {garageItem.calle} N° {garageItem.altura}
+                <br />
+                <strong>Descripción:</strong> {garageItem.descripcion}
+                <br />
+                <strong>Precio por hora:</strong> ${garageItem.preciohora}
+                <br />
+                <strong>Capacidad:</strong> {garageItem.capacidad} vehículos
+                <br />
+              </Typography>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  marginTop: '10px',
+                }}
+              >
                 <Button
                   variant="text"
                   color="primary"
                   onClick={() => navigate(`/historial-calificaciones/${garageItem.id}`)}
-                  style={{ textDecoration: 'underline', marginTop: '10px', marginRight: '10px' }}
+                  style={{
+                    textDecoration: 'underline',
+                    marginTop: '10px',
+                    marginRight: '10px',
+                  }}
                 >
                   Reseñas
                 </Button>
                 <Button
                   variant="contained"
                   color="primary"
-                  onClick={() => navigate(`/reserva/${garageItem.id}`)} // Aquí se pasa el ID del garaje
+                  onClick={() => navigate(`/reserva/${garageItem.id}`)}
                   style={{ marginTop: '10px' }}
                 >
                   Reservar
@@ -164,6 +201,19 @@ export default function HomeConductor() {
             </MainCard>
           ))}
       </div>
+    </Box>
+
+
+      {isDataLoaded && (
+        <Box style={{  flex: 4, height: '100%' }}>
+          
+          <MapView 
+          garajes={garaje.filter((garageItem) => garageItem.disponible)}
+          localidades={localidad}
+          provincias={provincia}
+          />
+        </Box>
+      )}
     </div>
   );
 }
