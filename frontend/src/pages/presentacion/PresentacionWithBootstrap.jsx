90fa05bf26c8ea22c11a981a7ba87e2e3e636da9
Nuria Robledo <nuriarobledo0@gmail.com>
2024-11-14 21:16:48 -0300

    feat: agrego el link para los iconos

--- frontend/src/pages/presentacion/PresentacionWithBootstrap.jsx
+++ frontend/src/pages/presentacion/PresentacionWithBootstrap.jsx
@@ -7,6 +7,11 @@ const PresentacionWithBootstrap = () => (
     <Helmet>
     <link rel="stylesheet" type="text/css" href="./public/css/bootstrap.css" /> 
     <link rel="stylesheet" type="text/css" href="./public/css/style.css" />
+    <link
+    rel="stylesheet"
+    type="text/css"
+    href="./public/fonts/font-awesome/css/font-awesome.css"
+  />
     </Helmet>
     <PresentacionPage />
   </>
