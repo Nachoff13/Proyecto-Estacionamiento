2317b9aa3ce0e6bbb1d2ec2b4bd5300d208839f5
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-11-15 23:30:45 -0300

    feat: traduje texto a español

--- frontend/src/pages/authentication/login.jsx
+++ frontend/src/pages/authentication/login.jsx
@@ -17,9 +17,9 @@ export default function Login() {
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-            <Typography variant="h3">Login</Typography>
+            <Typography variant="h3">Inicio de Sesión</Typography>
             <Typography component={Link} to="/register" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
-              Don&apos;t have an account?
+              No tienes cuenta?
             </Typography>
           </Stack>
         </Grid>
