8853bd03476186acc9494b83ba16064ce9991dec
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-11-15 23:30:59 -0300

    feat: traduje texto a español

--- frontend/src/pages/authentication/auth-forms/AuthLogin.jsx
+++ frontend/src/pages/authentication/auth-forms/AuthLogin.jsx
@@ -192,7 +192,7 @@ export default function AuthLogin({ isDemo = false }) {
               <Grid item xs={12}>
                 <AnimateButton>
                   <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary">
-                    Login
+                    Iniciar Sesión
                   </Button>
                 </AnimateButton>
               </Grid>
